e0aeff1fc7aa48866c3743c880eaa4f20280c94e
Adan Guanlao <aguanlaojr@gmail.com>
2020-04-07 14:11:02 -0700

    Added post creation via form with success/fail messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,18 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :description))
+    if @post.save
+      flash[:notice] = "Post created successfully."
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
 end
