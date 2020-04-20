8ece591de63745fc446bfe92424e69819850c8c1
Adan Guanlao <aguanlaojr@gmail.com>
2020-04-20 16:46:14 -0700

    Added user model and table

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :username, uniqueness: { case_sensitive: false }, 
+    presence: true, length: {minimum: 6, maximum: 25}
+  validates :email, uniqueness: { case_sensitive: false }, 
+    presence: true, length: {maximum: 128}, 
+    format: { with: VALID_EMAIL_REGEX }
+end
