b34c2a31157343d1bccc57099fee28b45d6d3564
Adan Guanlao <aguanlaojr@gmail.com>
2020-04-21 14:42:23 -0700

    Added hashed password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
   before_save { self.email = email.downcase }
+  has_secure_password
   has_many :posts
   validates :username, uniqueness: { case_sensitive: false }, 
     presence: true, length: {minimum: 6, maximum: 25}
