ebffeb3db87044e2e14faea646d94374627c66c8
Adan Guanlao <aguanlaojr@gmail.com>
2020-04-21 17:28:32 -0700

    Added create user route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    # Try to save new user & display success message, else show error
+    if @user.save
+      flash[:notice] = "Signed up successfully. Welcome #{@user.username}!"
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
