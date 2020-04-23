2134375b34cf585b3fe891661a473786fe1524e9
Adan Guanlao <aguanlaojr@gmail.com>
2020-04-23 13:34:09 -0700

    Added editing user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -9,13 +10,32 @@ class UsersController < ApplicationController
     # Try to save new user & display success message, else show error
     if @user.save
       flash[:notice] = "Signed up successfully. Welcome #{@user.username}!"
+      # TODO: Change redirect on success
       redirect_to posts_path
     else
       render 'new'
     end
   end
 
+  def edit
+  end
+
+  def update
+    # Try to update user & display proper feedback message
+    if @user.update(user_params)
+      flash[:notice] = "User was updated successfully."
+      # TODO: Change redirect on success
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end  
+
   private
+
+  def get_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
