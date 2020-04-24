09c9f8a1eacafa8affc6ab169cb6912937427f05
Adan Guanlao <aguanlaojr@gmail.com>
2020-04-23 18:05:01 -0700

    Added basic user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  before_action :get_user, only: [:edit, :update]
+  before_action :get_user, only: [:show, :edit, :update]
+
+  def show
+  end
 
   def new
     @user = User.new
