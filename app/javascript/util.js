b5f120808f9328b2ba3a24b92e789a556ab798e6
Adan Guanlao <aguanlaojr@gmail.com>
2020-05-18 17:59:36 -0700

    Added links for post interactions to popovers

--- app/javascript/util.js
+++ app/javascript/util.js
@@ -1,8 +1,16 @@
-$(function () {
-  $('[data-toggle="popover"]').popover({
-    html: true,
-    content: function () {
-      return $('#popover-content').html();
-    }
-  })
-})
+$(document).on('turbolinks:load', function () {
+  var popoverWhitelist = $.fn.tooltip.Constructor.Default.whiteList;
+
+  popoverWhitelist.a.push(...["rel", "data-confirm", "data-method"]);
+
+  $('[data-toggle="popover"]').each(function () {
+    var source = "div#" + $(this).attr("data-source");
+    $(this).popover({
+      html: true,
+      content: function () {
+        return $(source).html();
+      },
+      whiteList: popoverWhitelist
+    });
+  });
+});
