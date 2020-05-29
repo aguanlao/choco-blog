6e3484ed1fb98858b84faa6f8cdcd30050251224
Adan Guanlao <aguanlaojr@gmail.com>
2020-05-29 14:45:09 -0700

    Minor refactoring, grammar fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,17 +8,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Logged in successfully"
+      flash[:notice] = "Logged in successfully."
       redirect_to user
     else
-      flash.now[:error] = "Incorrect username and/or password"
+      flash.now[:error] = "Incorrect username and/or password."
       render 'new'      
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Logged out successfully"
+    flash[:notice] = "Logged out successfully."
     redirect_to root_path
   end
 
