4d441d6047b0d541591f9e75f105b8cbc4b9d3de
Adan Guanlao <aguanlaojr@gmail.com>
2020-06-02 17:44:23 -0700

    Created categories with unit tests

---
+++ test/models/category_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+  def setup
+    @category = Category.new(name: "Madagascar")
+  end
+
+  test "category must be valid" do
+    assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    @category2 = Category.new(name: "Madagascar")
+    assert_not @category2.valid?
+  end
+
+  test "name should be shorter than 25" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name should be longer than 3" do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+end
