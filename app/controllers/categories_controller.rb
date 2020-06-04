19867e1f1d65fe3975e9929d94154327ba6d0b74
Adan Guanlao <aguanlaojr@gmail.com>
2020-06-04 14:29:56 -0700

    Added categories with basic controller & views

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,38 @@
+class CategoriesController < ApplicationController
+  before_action :get_category, only: [:show]
+  
+  def show
+  end
+  
+  def index
+  end
+  
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category created successfully."
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def get_category
+    begin
+      @category = Category.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "Category not found."
+      redirect_to posts_path
+    end
+  end
+end
