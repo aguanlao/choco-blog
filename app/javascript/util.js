8a0818a5d105c7b46671095ca9de8e9e5021d8aa
Adan Guanlao <aguanlaojr@gmail.com>
2020-07-02 17:19:44 -0700

    Added copying post link to clipboard on sharing

--- app/javascript/util.js
+++ app/javascript/util.js
@@ -1,16 +1,67 @@
-$(document).on('turbolinks:load', function () {
+$(document).on('turbolinks:load', function() {
   var popoverWhitelist = $.fn.tooltip.Constructor.Default.whiteList;
 
+  // Must whitelist attributes for popovers' elements
+  popoverWhitelist.button = ["data-copy-text", "data-target"];
   popoverWhitelist.a.push(...["rel", "data-confirm", "data-method"]);
 
-  $('[data-toggle="popover"]').each(function () {
+  $('[data-toggle="popover"]').each(function() {
     var source = "div#" + $(this).attr("data-source");
     $(this).popover({
       html: true,
-      content: function () {
+      content: function() {
         return $(source).html();
       },
       whiteList: popoverWhitelist
     });
   });
+
+  $('[data-toggle="popover"]').click(() => {
+    // Bind copy button to toggle result tooltip with proper message
+    $('[data-target^="copy"]').click(function() {
+      var tooltipText = $(this).data("copyText");
+      var tooltipTarget = "#" + $(this).attr("data-target");
+      var copyResult = copyText(tooltipText);
+      var message = copyResult ? "Link copied to clipboard" : "Copy failed";
+
+      $(tooltipTarget).attr("data-original-title", message).tooltip('toggle');
+      setTimeout(() => $(tooltipTarget).tooltip('hide'), 1000);
+    });
+  });
+  
+  $('[data-toggle="copy-tooltip"]').tooltip({
+    template: '<div class="tooltip copy-tooltip" role="tooltip"><div class="tooltip-inner"></div></div>',
+    trigger: "manual",
+    offset: -100
+  });
 });
+
+function copyText(text) {
+  var textArea = document.createElement("textarea");
+  var successful;
+
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+  textArea.style.padding = 0;
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+  textArea.style.background = 'transparent';
+
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  try {
+    successful = document.execCommand('copy');
+  } catch (err) {
+    console.error('Failed to copy text to clipboard.');
+  }
+
+  document.body.removeChild(textArea);
+  return successful;
+}
